d9efe318ab4b3cb640d1e64cb2d4d89cf0992703
YASIN <56336030+Yasin-13@users.noreply.github.com>
2024-08-13 10:10:38 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Honk } from "next/font/google";
 
-const honk = ({ subsets: ["latin"] });
 const inter = Inter({ subsets: ["latin"] });
+
 export const metadata = {
   title: "YASIN inc",
   description: "portfolio",
@@ -13,7 +12,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        
+        {/* Matomo Analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _paq = window._paq = window._paq || [];
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://cybersec13.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src='https://cdn.matomo.cloud/cybersec13.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            `,
+          }}
+        />
+        {/* End Matomo Analytics */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
